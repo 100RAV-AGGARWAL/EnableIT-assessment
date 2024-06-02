b97d75c961a8a45ee5501082256609e1f498fb7c
100RAV-AGGARWAL <81032655+100RAV-AGGARWAL@users.noreply.github.com>
2024-06-02 11:47:53 +0530

    github link added

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -22,8 +22,16 @@ const UserList: React.FC = () => {
             const response = await axios.get(
                 `https://give-me-users-forever.vercel.app/api/users/${currentPage}/next`
             );
-            setUsers(response.data.users === "No users Left" ? [] : response.data.users);
-            setTotalPages(response.data.users === "No users Left" ? currentPage : currentPage + 20);
+            setUsers(
+                response.data.users === "No users Left"
+                    ? []
+                    : response.data.users
+            );
+            setTotalPages(
+                response.data.users === "No users Left"
+                    ? currentPage
+                    : currentPage + 20
+            );
         };
 
         fetchUsers();
@@ -67,7 +75,21 @@ const UserList: React.FC = () => {
         //     />
         // </div>
         <div className="container mx-auto p-4 h-screen flex flex-col">
-            <h1 className="text-2xl font-bold mb-4">User List</h1>
+            <div className="flex justify-between mb-4">
+                <div className="w-full"></div>
+                <h1 className="text-2xl font-bold w-full">User List</h1>
+                <div className="w-full flex justify-end">
+                    <button className="px-2 py-1 bg-gray-800 text-white rounded">
+                        <a
+                            href="https://github.com/100RAV-AGGARWAL/EnableIT-assessment"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Github
+                        </a>
+                    </button>
+                </div>
+            </div>
             <div className="shadow-lg p-5 rounded flex-1 overflow-auto">
                 {users && users.length > 0 ? (
                     <ul className="space-y-4">
